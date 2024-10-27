c637a33ea31731003995ae4cffc84770b4a29058
Diyakaneriya <115219377+Diyakaneriya@users.noreply.github.com>
2024-10-27 23:40:48 +0530

    Time: 7 ms (45.12%), Space: 55 MB (46.05%) - LeetHub

---
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int totalSquares = 0;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;  // Edge cells can only form a 1x1 square
+                    } else {
+                        dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
+                    }
+                    totalSquares += dp[i][j];
+                }
+            }
+        }
+        
+        return totalSquares;
+    }
+}
